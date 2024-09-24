c943982348aa81d89f9de04864e85c03f0e24576
MuhammadJunaidHunani <junaidhunani890@gmail.com>
2024-09-24 12:27:41 +0500

    create Projects design

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,34 +1,40 @@
-
 // import quiz from '../../assets/images/projects/quiz.png'
 // import clock from '../../assets/images/projects/clock.png'
-import slider from '../../assets/images/projects/slider.png'
-import chatApp from '../../assets/images/projects/chatApp.png'
-import lift from '../../assets/images/projects/lift.png';
-import styles from './style.module.css';
+import slider from "../../assets/images/projects/slider.png";
+import chatApp from "../../assets/images/projects/chatApp.png";
+import lift from "../../assets/images/projects/lift.png";
+import styles from "./style.module.css";
 
 const myProjects = [
   {
-    title: 'Lift Algorithm',
-    url: 'liift.netlify.app',
-    img:slider ,
-  }
-]
-const Projects = ()=>{
-    
-    return(
-        <div className={styles.main} id="projects">
-            <div className={styles.projectsCont}>
-                {myProjects.map((item, index)=>{
-                    return(
-                        <div className={styles.projectCard}>
-                            {/* <p className={styles.cardTitle}>{item.title}</p> */}
-                            <img className={styles.cardImg} src={item.img} alt={item.title} />
-                            <div className={styles.behindLay}></div>
-                        </div>
-                    )
-                })}
+    title: "Lift Algorithm",
+    url: "liift.netlify.app",
+    img: slider,
+  },
+];
+const Projects = () => {
+  return (
+    <div className={styles.main} id="projects">
+      <div className={styles.projectsCont}>
+        {myProjects.map((item, index) => {
+          return (
+            <div className={styles.projectCard}>
+              {/* <p className={styles.cardTitle}>{item.title}</p> */}
+              <div className={styles.imgDescCont}>
+                <img
+                  className={styles.cardImg}
+                  src={item.img}
+                  alt={item.title}
+                />
+                <div className={styles.prjDesc}></div>
+              </div>
+              <div className={styles.behindLay}></div>
+              <div className={styles.behindLay2}></div>
             </div>
-       </div>
-    )
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 export default Projects;
