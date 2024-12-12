fe0d632f98eff1e8bc0689f7b65c422f3cee51eb
MuhamadJunaidHunani <junaidhunani890@gmail.com>
2024-12-13 00:51:17 +0500

    working on portfolio

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import styles from "./style.module.css";
 
 const NavBar = (props) => {
-    const [activeLang , setActiveLink] = useState(0);
+  const [activeLang, setActiveLink] = useState(0);
 
   return (
     <div className={styles.main}>
-        {props.languages.map((lang, index) => (
-          <div key={index} className={`${styles.langCont} ${activeLang === index? styles.activeLang:''}`} onMouseEnter={()=>setActiveLink(index)}>
-            <img src={lang.icon} alt="icon" />
-             <p>{lang.name}</p>
-          </div>
-        ))}
-      </div>
+      {props.languages.map((lang, index) => (
+        <div key={index} className={`${styles.langCont} ${activeLang === index ? styles.activeLang : ''}`} onMouseEnter={() => setActiveLink(index)}>
+          <img src={lang.icon} alt="icon" />
+          <p>{lang.name}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 export default NavBar;
